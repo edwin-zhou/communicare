bbe327b157ed29da7690d4c1e8212002c185d86f
SimonTheWang <simonwg2001@gmail.com>
2020-09-05 17:34:43 -0400

    made fixes to login and register

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 
 const routes: Routes = [
-  { path: "welcome-page", component: WelcomePageComponent }
+  { path: "welcome-page", component: WelcomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegistrationPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
